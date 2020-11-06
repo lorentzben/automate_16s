0f06721a0b4593310cc1d01b6af2880fdb59a3f6
Ben Lorentz <bjl34716@uga.edu>
2020-11-06 16:24:31 -0500

    working on the logic for the manifest checking/fastq discovery

--- setup.py
+++ setup.py
@@ -4,6 +4,8 @@ from subprocess import PIPE
 import os
 from os import path
 import logging
+from pathlib import Path
+from pathlib import PurePath
 
 def setup_logging():
     logger = logging.getLogger(__name__)
@@ -26,28 +28,42 @@ def setup_logging():
     logger.addHandler(console_logger)
 
 def check_dependencies():
-    #checks to see if nextflow is installed
+    #checks to see if nextflow is installed, version is saved to log
     try:
         command = subprocess.run(['nextflow -v'], stdout=PIPE, stderr=PIPE)
         logger.info("nextflow installed: " + command.stdout)
     except FileNotFoundError:
         logger.critical("It appears nextflow is not installed or loaded")
         exit(1)
-
+    #checks to see if qiime is installed, all software versions are saved to log
     try:
         command = subprocess.run(['qiime info'], stdout=PIPE, stderr=PIPE)
         logger.info(command.stdout)
     except FileNotFoundError:
         logger.critical("It appears qiime2 is not installed or loaded")
         exit(1)
-    
     logger.info('all software installed and ready to go')
 
 def verify_manifest(mainfest):
-    print("the manifest called: " +manifest+ " is valid and ready to go")
+    read_manifest = pd.read_csv(maifest, index_col=0,)
 
-def verify_files(manifest):
-    print("the files from the manifest have been accounted for")
+    p = Path.cwd()
+    list_of_fastq = list(p.glob('**/*.fastq'))
+    list_of_gz = list(p.glob('**/*.fastq.gz'))
+    found = []
+    not_found = []
+
+    for item in list_of_fastq:
+        print(item)
+        found.append(item)
+    for item in list_of_gz:
+        print(item)
+        found.append(item)
+
+    if len(found) not len(read_manifest):
+        logger.critical("The files in the manifest were not found in this directory")
+    else:
+        logging.info("the manifest called: " +manifest+ " is valid and ready to go")
 
 def error():
     print("something is wrong and I will try to tell you what the problem is")
