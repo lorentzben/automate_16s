25cfaaec4bb73cceb127a1ebe63aa6c231ef241f
Ben Lorentz <bjl34716@uga.edu>
2020-11-09 10:47:15 -0500

    error

--- setup.py
+++ setup.py
@@ -93,7 +93,7 @@ def verify_manifest(mainfest):
 
     # if missing is not an empty list, i.e. a file listed in the manifest is not detected, it raises an error and 
     # creates a list for the user
-    if missing not []:
+    if missing != []:
 
         logger.critical("files are missing, please see missing.csv to correct")
 
