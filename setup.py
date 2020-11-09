85d85b115f7b28553fa3087509c1def544146af8
Ben Lorentz <bjl34716@uga.edu>
2020-11-09 10:56:31 -0500

    changed language of error messages

--- setup.py
+++ setup.py
@@ -34,14 +34,14 @@ def check_dependencies():
         command = subprocess.run(['nextflow -v'], stdout=PIPE, stderr=PIPE)
         logger.info("nextflow installed: " + command.stdout)
     except FileNotFoundError:
-        logger.critical("It appears nextflow is not installed or loaded")
+        logger.info("It appears nextflow is possibly not installed or loaded, but this error appears when everything works ok too")
         #exit(1)
     #checks to see if qiime is installed, all software versions are saved to log
     try:
         command = subprocess.run(['qiime info'], stdout=PIPE, stderr=PIPE)
         logger.info(command.stdout)
     except FileNotFoundError:
-        logger.critical("It appears qiime2 is not installed or loaded")
+        logger.info("It appears qiime2 is possibly not installed or loaded, but this error appears when everything works ok too")
         #exit(1)
     logger.info('all software installed and ready to go')
 
@@ -116,4 +116,4 @@ def error():
 
 check_dependencies()
 # TODO can come back and make this user-editable, but for right now I will hard-code it. 
-verify_manifest('manifest.tsv')
+verify_manifest("manifest.tsv")
