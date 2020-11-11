552de81eb4418b8d58a50cbee26f1b53e9cd64be
Ben Lorentz <bjl34716@uga.edu>
2020-11-11 15:39:51 -0500

    read_table

--- automate_16.py
+++ automate_16.py
@@ -66,7 +66,7 @@ def qual_control():
 
 def calc_qual_cutoff():
     input_file = 'inflate/*/data/forward-seven-number-summaries.tsv'
-    summary = pd.read_tsv(input_file, index_col=0,sep='\t')
+    summary = pd.read_table(input_file, index_col=0,sep='\t')
 
     mean_qual = summary[4:5]
 
