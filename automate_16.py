a40eb3cc3065b08c757bfe92fd99f0955753b192
Ben Lorentz <bjl34716@uga.edu>
2020-11-12 16:16:43 -0500

    added more calcs and qiime calls

--- automate_16.py
+++ automate_16.py
@@ -115,17 +115,97 @@ def calc_qual_cutoff():
 def call_denoise(right, left, seq_format):
     if seq_format == 'single':
         command = "qiime dada2 denoise-single --i-demultiplexed-seqs demux.qza --p-trim-left " + str(left)+" --p-trunc-len " + \
-            str(right) + " --o-representative-sequences rep_seqs-dada2.qza --o-table table-dada2.qza --o-denoising-stats stats-dada2.qza"
+            str(right) + " --o-representative-sequences rep-seqs-dada2.qza --o-table table-dada2.qza --o-denoising-stats stats-dada2.qza"
     elif seq_format == 'paired':
         command = "qiime dada2 denoise-paired --i-demultiplexed-seqs demux.qza --p-trim-left " + str(left)+" --p-trunc-len " + \
-            str(right) + " --o-representative-sequences rep_seqs-dada2.qza --o-table table-dada2.qza --o-denoising-stats stats-dada2.qza"
-    result = subprocess.run([command], stdout=PIPE, stderr=PIPE, shell=True)
+            str(right) + " --o-representative-sequences rep-seqs-dada2.qza --o-table table-dada2.qza --o-denoising-stats stats-dada2.qza"
 
+    result = subprocess.run([command], stdout=PIPE, stderr=PIPE, shell=True)
+    logger.info(result.stdout)
+    logger.critical(result.stderr)
+
+    command = "qiime metadata tabulate --m-input-file stats-dada2.qza --o-visualization stats-dada2.qza"
+
+    result = subprocess.run([command], stdout=PIPE, stderr=PIPE, shell=True)
     logger.info(result.stdout)
     logger.critical(result.stderr)
 
 
+def feature_visualizations(metadata):
+    command = "qiime feature-table summarize --i-table table-dada2.qza --o-visualization table.qzv --m-sample-metadata-file " + metadata
+    result = subprocess.run([command], stdout=PIPE, stderr=PIPE, shell=True)
+    logger.info(result.stdout)
+    logger.critical(result.stderr)
+
+    command = "qiime feature-table tabulate-seqs --i-data rep-seqs-dada2.qza --o-visualization rep-seqs.qzv"
+    result = subprocess.run([command], stdout=PIPE, stderr=PIPE, shell=True)
+    logger.info(result.stdout)
+    logger.critical(result.stderr)
+
+
+def tree_construction():
+    command = "qiime align-to-tree-mafft-fasttree --i-sequences rep-seqs-dada2.qza --o-alignment aligned-rep-seqs.qza --o-masked-alignment masked-aligned-rep-seqs.qza --o-tree unrooted-tree.qza --o-rooted-tree rooted-tree.qza"
+    result = subprocess.run([command], stdout=PIPE, stderr=PIPE, shell=True)
+    logger.info(result.stdout)
+    logger.critical(result.stderr)
+
+
+def determine_depth():
+    command = "unzip -d inflate table.qzv"
+    result = subprocess.run([command], stderr=PIPE, stdout=PIPE, shell=True)
+
+    logger.info(result.stdout)
+    logger.critical(result.stderr)
+
+    input_file = glob.glob(
+        './inflate/*/data/sample-frequency-detail.csv')
+   
+
+    features = pd.read_csv(input_file[0], index_col=0,header=None)
+
+    total_count = sum(features[1])
+    sampling_depth = 0 
+    perc_features_retain = 0.0
+
+    print("total count" + str(total_count))
+
+    feature_array = np.array(features)
+
+    for i in range(len(feature_array)-1, -1,-1):
+        sampling_depth = feature_array[i][0]
+        perc_features_retain = ((sampling_depth * (i+1))/total_count)
+        if perc_features_retain > .22:
+            sampling_depth = feature_array[i][0]
+            print("sampling depth: " + str(sampling_depth) + " % features retained: " + str(round(perc_features_retain,3)) + " samples retained: " + str(i))
+            break
+
+
+    with open('sampling_depth.csv', 'w', newline='') as csvfile:
+        fieldnames = ['stat', 'value']
+        writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
+
+        writer.writeheader()
+        writer.writerow({'stat': 'sampling_depth' , 'value': sampling_depth})
+        writer.writerow({'stat': '%_features_retained', 'value': perc_features_retain})
+        writer.writerow({'stat': 'filename', 'value': input_file})
+
+    logger.info("sampling_depth: "+str(sampling_depth))
+    logger.info("%_features_retained: " +str(perc_features_retain))
+    
+    return(sampling_depth)
+
+def diversity_measure(metadata, depth):
+    command = "qiime diversity core-metrics-phylogenetic --i-phylogeny rooted-tree.qza --i-table table.qza --p-sampling-depth " + \
+        depth + " --m-metadata-file " + metadata + " --output-dir core-metrics-results"
+    result = subprocess.run([command], stdout=PIPE, stderr=PIPE, shell=True)
+    logger.info(result.stdout)
+    logger.critical(result.stderr)
+
+    
+
 # TODO put checks in to pickeup from where a failed run left off.
+
+
 def main(arg):
     single_or_pair = single_or_paired_read(arg.manifest_name)
 
@@ -144,6 +224,14 @@ def main(arg):
 
     call_denoise(right_cutoff, left_cutoff, single_or_pair)
 
+    feature_visualizations(arg.metadata)
+
+    tree_construction()
+
+    depth = determine_depth()
+
+    diversity_measure(arg.metadata, depth)
+
 
 if __name__ == "__main__":
     # Build Argument Parser in order to facilitate ease of use for user
@@ -151,6 +239,8 @@ if __name__ == "__main__":
         description="Checks dependencies are installed, and validates a manifest file for qiime 2")
     parser.add_argument('-n', '--name', action='store', required=True,
                         help="name for the manifest, typically manifest.tsv", dest='manifest_name')
+    parser.add_argument('-m', '--metadata', action='store', required=True,
+                        help="name of the metadata file, usually metadata.tsv", dest='metadata')
     parser.add_argument('-v', '--version', action='version',
                         version='%(prog)s 1.0')
 
