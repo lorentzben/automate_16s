27d9162f8cca5f5c9c2ac54c5fff34369cf4f257
Ben Lorentz <bjl34716@uga.edu>
2020-11-13 10:27:34 -0500

    added done for debugging

--- automate_16.py
+++ automate_16.py
@@ -221,7 +221,7 @@ def alpha_div_calc(metadata):
 def beta_div_calc(metadata, item_of_interest):
     command = "qiime diversity beta-group-significance --i-distance-matrix core-metrics-results/unweighted_unifrac_distance_matrix.qza --m-metadata-file " + \
         metadata + " --m-metadata-column "+item_of_interest + \
-        " --o-visualization core-metrics-results/unwighted-unifrac-body-site-significance.qzv --p-pairwise"
+        " --o-visualization core-metrics-results/unweighted-unifrac-body-site-significance.qzv --p-pairwise"
     result = subprocess.run([command], stdout=PIPE, stderr=PIPE, shell=True)
     logger.info(result.stdout)
     logger.critical(result.stderr)
@@ -259,6 +259,8 @@ def main(arg):
 
     if arg.interest:
         beta_div_calc(arg.metadata, arg.interest)
+
+    logger.info('done')
 
 
 if __name__ == "__main__":
