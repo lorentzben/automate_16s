dfdfb6cd23ced513098fd150f1c7f0a713f6b7c7
Ben Lorentz <bjl34716@uga.edu>
2020-11-16 10:33:12 -0500

    changed vars

--- helper_bash.sh
+++ helper_bash.sh
@@ -1,16 +1,16 @@
 #!/usr/bin/env bash
-"manifest.tsv" = $manifest
-$metadata = "metadata.tsv"
-$seqs = "seq"
+MANIFEST="manifest.tsv"
+METADATA="metadata.tsv"
+SEQS="seq"
 
 #this must be a column name in the metadata.tsv file 
-$var_of_interest = "treatment"
+VAR_OF_INTEREST="treatment"
 
 module load Python/3.8.2-GCCcore-8.3.0
 module load Nextflow/20.04.1
 module load QIIME2/2020.6
 
 python3 -m pip install --user pandas
-python3 setup.py -n $manifest -d $seqs
+python3 setup.py -n $MANIFEST -d $SEQS
 
-python3 automate_16.py -n $manifest -m $metadata -i $var_of_interest
+python3 automate_16.py -n $MANIFEST -m $METADATA -i $VAR_OF_INTEREST
