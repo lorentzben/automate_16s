f695e586e9c4e544d2b4bc91727bc6faf5bb1619
Ben Lorentz <bjl34716@uga.edu>
2020-11-17 09:39:24 -0500

    change core subfile

--- automate_16.py
+++ automate_16.py
@@ -238,7 +238,7 @@ def beta_div_calc(metadata, item_of_interest):
         'calculating beta diversity, only done if column of metadata is provided')
     command = "qiime diversity beta-group-significance --i-distance-matrix core-metrics-results/unweighted_unifrac_distance_matrix.qza --m-metadata-file " + \
         metadata + " --m-metadata-column "+item_of_interest + \
-        " --o-visualization core-metrics-results/unweighted-unifrac-body-site-significance.qzv --p-pairwise"
+        " --o-visualization core-metrics-results/unweighted-unifrac-"+item_of_interest+"-significance.qzv --p-pairwise"
     result = subprocess.run([command], stdout=PIPE, stderr=PIPE, shell=True)
     logger.info(result.stdout)
     logger.critical(result.stderr)
