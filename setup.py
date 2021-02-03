3088ea03a9f6961edcd39467c062c1113fa03079
Ben Lorentz <bjl34716@uga.edu>
2021-02-03 11:02:28 -0500

    debugging:

--- setup.py
+++ setup.py
@@ -57,11 +57,10 @@ def check_dependencies():
             logger.critical(
                 "a conda environment named \'python2\' does not exist; execute \'conda create -n python2 python=2.7.17\'")
 
-    command = subprocess.run(["./conda_env_depends.sh"],
-                             stdout=PIPE, stderr=PIPE, shell=True)
-    logger.info(command.stdout)
-    if command.returncode != 0:
-        logger.error(command.stederr)
+    result = subprocess.run(["./conda_env_depends.sh"], stdout=PIPE, stderr=PIPE, shell=True)
+    logger.info(result.stdout)
+    if result.returncode != 0:
+        logger.error(result.stederr)
         logger.critical("Error installing packages in conda environment")
 
     logger.info('all software installed and ready to go')
