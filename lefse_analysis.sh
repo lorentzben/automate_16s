e9f3ad8c5e706ab22cc24c9c00a481e1f4d1e9f7
Ben Lorentz <bjl34716@uga.edu>
2021-02-16 16:13:54 -0500

    change graph

--- lefse_analysis.sh
+++ lefse_analysis.sh
@@ -12,6 +12,6 @@ do
     #TODO use export2graphlan to make a better cladogram
     export2graphlan.py -i lefse_formatted.txt -o lefse_result.res -t tree.nwk -a annot_lefse.txt --external_annotations 2,3,4,5,6 --fname_row 0 --skip_rows 1 
     graphlan_annotate.py --annot annot_lefse.txt tree.nwk annotated_tree_lefse.txt
-    graphlan.py annotated_tree_lefse.txt "${eachfile::-4}"_better_cladogram.png --format png --size 16 --dpi 300 --pad 0.2
+    graphlan.py annotated_tree_lefse.txt "${eachfile::-4}"_better_cladogram.png --format png --size 10 --dpi 300
 done
 conda deactivate
