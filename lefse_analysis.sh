92946850c09ddbfcd6ee6076ba657dcd019a0c8f
Ben Lorentz <bjl34716@uga.edu>
2021-02-24 09:57:51 -0500

    added the call for lefse og cladogam plotting

--- lefse_analysis.sh
+++ lefse_analysis.sh
@@ -8,7 +8,7 @@ do
     format_input.py $eachfile lefse_formatted.in -c 2 -u 1
     run_lefse.py lefse_formatted.in lefse_result.res 
     plot_res.py lefse_result.res "${eachfile::-4}"_res.png --dpi 150 
-    plot_cladogram.py lefse_result.res "${eachfile::-4}"_cladogram.png --format png
+    plot_cladogram.py lefse_result.res "${eachfile::-4}"_cladogram.png --format png --dpi 300
     #TODO use export2graphlan to make a better cladogram
     export2graphlan.py -i lefse_formatted.txt -o lefse_result.res -t tree.nwk -a annot_lefse.txt --external_annotations 2,3,4,5,6 --fname_row 0 --skip_rows 1 
     graphlan_annotate.py --annot annot_lefse.txt tree.nwk annotated_tree_lefse.txt
